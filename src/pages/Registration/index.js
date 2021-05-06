8bf6caf40cd6292d4e461389cdb2276508cc0bcc
Harshadi <46489854+Harshadi@users.noreply.github.com>
2021-05-06 12:35:14 +0530

    Add files via upload

---
+++ src/pages/Registration/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './styles.scss';
+import Signup from './../../components/Signup';
+
+class Registration extends Component {
+    
+    render(){
+    return (
+        <Signup />
+    )
+}
+}
+
+export default Registration 
