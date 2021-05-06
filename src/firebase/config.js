0eaa39eae07783046e42a036ff073931bc295f97
Harshadi <46489854+Harshadi@users.noreply.github.com>
2021-05-06 12:28:55 +0530

    Add files via upload

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCD8i6hxDOy1UwJigB8egT7NpyzvQK0f-A",
+  authDomain: "e---commerce---app.firebaseapp.com",
+  projectId: "e---commerce---app",
+  storageBucket: "e---commerce---app.appspot.com",
+  messagingSenderId: "510501702679",
+  appId: "1:510501702679:web:9d2ebc329fc222ca66e052",
+  measurementId: "G-DWPQKYKK8Y"
+}
